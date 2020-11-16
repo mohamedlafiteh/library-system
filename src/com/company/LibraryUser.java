cceff66a14496663949461966ab1994863a99a89
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-11-16 21:40:05 +0000

    Add member class

--- src/com/company/LibraryUser.java
+++ src/com/company/LibraryUser.java
@@ -3,14 +3,91 @@ package com.company;
 import java.util.ArrayList;
 
 public class LibraryUser {
-    String firstName;
-    String lastName;
-    String notifications;
-    ArrayList<Book> borrowedBooks;
-    public LibraryUser(String firstName,String lastName){
+    private  String firstName;
+    private  String lastName;
+    private  String id;
+    private  String notifications;
+  ArrayList<Book> borrowedBooks;
+
+    public LibraryUser(String firstName,String lastName,String id){
         this.firstName=firstName;
         this.lastName=lastName;
+        this.id=id;
         this.notifications="";
         borrowedBooks=new ArrayList<>();
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(String notifications) {
+        this.notifications += notifications;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void borrowBook(Book book){
+        borrowedBooks.add(book);
+    }
+    public void returnBook(Book book){
+        borrowedBooks.remove(book);
+    }
+    public void printUserDetails(){
+        String details="";
+        details+="The first name is " + firstName + "\n";
+        details+="The last name is " + lastName + "\n";
+        details+="The Id is " + id + "\n";
+        System.out.println(details);
+    }
+    public void printUserMessages(){
+        System.out.println(notifications);
+    }
+    public void addBook(Book book){
+        borrowedBooks.add(book);
+    }
+
+    public String printAllBooksInList(){
+        String bookDetails="";
+        if(borrowedBooks.size()>0){
+
+            for (Book book:borrowedBooks){
+                bookDetails+="ISBN " + book.getISBN()+"\n";
+                bookDetails+="bookTitle " +book.getBookTitle()+"\n";
+                bookDetails+="author " + book.getAuthor()+"\n";
+                bookDetails+="year " + book.getYear()+"\n";
+                bookDetails+="publisher " + book.getPublisher()+"\n";
+
+            }
+        }else {
+            bookDetails+="Sorry the list is empty ";
+        }
+        return bookDetails;
+    }
+    public int numberOfBorrowedBooks(){
+        return borrowedBooks.size();
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,22 +6,36 @@ public class Main {
 	// write your code here
         Library library = new Library();
         LibraryElectronicDevices libraryDevice=new LibraryElectronicDevices();
+        Book book1=new Book(10,"JAVA","BlueJ",2020,"SeaCode");
+        Book book2=new Book(20,"python","red",1920,"Code");
+
+        LibraryUser member1=new LibraryUser("Mohamed","Ali","2256");
+//        library.addBook(book1);
+//        library.addBook(book2);
+         member1.borrowBook(book1);
+         member1.borrowBook(book2);
+        System.out.println(member1.borrowedBooks.size());
+        System.out.println(member1.printAllBooksInList());
 
 
-        LibraryElectronicDevices laptop=new LibraryElectronicDevices("computer","floor1",true);
-        LibraryElectronicDevices ipad=new LibraryElectronicDevices("tablet"," floor2",true);
-        LibraryElectronicDevices phone=new LibraryElectronicDevices("tablet"," floor2",true);
 
 
-        libraryDevice.addDevices(laptop);
-        libraryDevice.addDevices(ipad);
-        System.out.println(libraryDevice.devicesInLibrary.size());
-        System.out.println(libraryDevice.printDeviceDetails(ipad));
 
-        System.out.println(libraryDevice.checkAvailability(phone));
-        System.out.println(libraryDevice.printDeviceDetails(phone));
-        //System.out.println(libraryDevice.getDeviceAvailability());
-        System.out.println(phone.getDeviceAvailability());
+
+//        LibraryElectronicDevices laptop=new LibraryElectronicDevices("computer","floor1",true);
+//        LibraryElectronicDevices ipad=new LibraryElectronicDevices("tablet"," floor2",true);
+//        LibraryElectronicDevices phone=new LibraryElectronicDevices("tablet"," floor2",true);
+//
+//
+//        libraryDevice.addDevices(laptop);
+//        libraryDevice.addDevices(ipad);
+//        System.out.println(libraryDevice.devicesInLibrary.size());
+//        System.out.println(libraryDevice.printDeviceDetails(ipad));
+//
+//        System.out.println(libraryDevice.checkAvailability(phone));
+//        System.out.println(libraryDevice.printDeviceDetails(phone));
+//        //System.out.println(libraryDevice.getDeviceAvailability());
+//        System.out.println(phone.getDeviceAvailability());
 
 
 
