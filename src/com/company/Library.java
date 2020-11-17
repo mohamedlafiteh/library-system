054b0bbdaaa9ee4a174c829a2b06cde32de22acc
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-11-17 23:57:00 +0000

    Add more methods to the libraray class

--- src/com/company/Library.java
+++ src/com/company/Library.java
@@ -4,9 +4,13 @@ import java.util.ArrayList;
 
 public class Library {
     ArrayList<Book> resources;
+    ArrayList<LibraryElectronicDevices> devices;
+    ArrayList<LibraryUser> users;
 
     public Library() {
         resources = new ArrayList<>();
+        devices=new ArrayList<>();
+        users =new ArrayList<>();
     }
 
     public void addBook(Book book) {
@@ -20,11 +24,14 @@ public class Library {
         String bookInf = "";
 
         for (int i = 0; i < resources.size(); i++) {
-            // bookInf += "book title " + resources.get(i).bookTitle;
+
             if (resources.size() > 0) {
-                 bookInf+= resources.get(i).getBookTitle();
+                 bookInf+= resources.get(i).getBookTitle() +"\n";
                // bookInf+= resources.get(i).getDamages();
-                bookInf+= resources.get(i).getISBN();
+                bookInf+= resources.get(i).getISBN() +"\n";
+                bookInf+= resources.get(i).getAuthor() +"\n";
+                bookInf+= resources.get(i).getYear() +"\n";
+                bookInf+= resources.get(i).getPublisher() +"\n" +"\n";
 
             } else {
                  bookInf += "the library is empty ";
@@ -42,6 +49,78 @@ public class Library {
              }
     }
     public void editBookTitle(Book book, String newTitle){
+        if(resources.contains(book)){
+            int index = resources.indexOf(book);
+            Book bookToEdit= resources.get(index);
+            bookToEdit.setBookTitle(newTitle);
+        }else {
+            System.out.println("The book is not in the library");
+        }
+    }
 
+    public Book searchBookInResourcesByObject(Book book){
+        if(resources.contains(book)){
+            int index = resources.indexOf(book);
+            Book bookFound= resources.get(index);
+          return bookFound;
+        }else {
+            return null;
+        }
+    }
+
+    public String searchBookInResourcesByISBN(int isbn){
+        int count=0;
+        String details="";
+      for (Book book:resources){
+          if(book.getISBN()==isbn){
+             details+= book.getBookTitle() +"\n";
+              details+= book.getISBN() +"\n";
+              details+= book.getBookTitle() +"\n";
+              details+= book.getAuthor() +"\n";
+              details+= book.getYear() +"\n";
+              details+= book.getPublisher() +"\n";
+              count++;
+          }else {
+              details+="No resources with this isbn";
+          }
+      }
+        System.out.println("The number or books with this " + isbn + " is " +count);
+        return details;
+    }
+
+    public String searchBookInResourcesByAuthorName(String authorName){
+        int count=0;
+        String details="";
+        for (Book book:resources){
+            if(book.getAuthor().equals(authorName)){
+                details+= book.getBookTitle() +"\n";
+                details+= book.getISBN() +"\n";
+                details+= book.getBookTitle() +"\n";
+                details+= book.getAuthor() +"\n";
+                details+= book.getYear() +"\n";
+                details+= book.getPublisher() +"\n";
+                count++;
+            }else {
+                details+="No resources with this author name";
+            }
+        }
+        System.out.println("The number or books with this author name " + authorName + " is " +count);
+        return details;
+    }
+
+    public void removeResource(Book book){
+        if(resources.contains(book)){
+           resources.remove(book);
+        }else {
+            System.out.println("Not available in the library");
+        }
+    }
+    public void removeResourceByPosition(int position){
+        Book bookFound= resources.get(position);
+        if(resources.contains(bookFound)){
+            resources.remove(position);
+        }else {
+            System.out.println("Not available in the library using position");
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,17 +6,30 @@ public class Main {
 	// write your code here
         Library library = new Library();
         LibraryElectronicDevices libraryDevice=new LibraryElectronicDevices();
-        Book book1=new Book(10,"JAVA","BlueJ",2020,"SeaCode");
-        Book book2=new Book(20,"python","red",1920,"Code");
+        Book book1=new Book(22,"JAVA","BlueJ",2020,"SeaCode");
+        Book book2=new Book(22,"python","BlueJ",1920,"Code");
+        Book book3=new Book(20,"python","red",1920,"Code");
+
 
         LibraryUser member1=new LibraryUser("Mohamed","Ali","2256");
-//        library.addBook(book1);
-//        library.addBook(book2);
-         member1.borrowBook(book1);
-         member1.borrowBook(book2);
-        System.out.println(member1.borrowedBooks.size());
-        System.out.println(member1.printAllBooksInList());
+        library.addBook(book1);
+        library.addBook(book2);
+        System.out.println(library.libraryDetails());
+        library.removeResourceByPosition(1);
+        System.out.println(library.libraryDetails());
+//         member1.borrowBook(book1);
+//         member1.borrowBook(book2);
 
+//        System.out.println(member1.borrowedBooks.size());
+//        System.out.println(member1.printAllBooksInList());
+
+        //System.out.println(library.libraryDetails());
+        //library.editBookTitle(book2,"c++");
+        //System.out.println(library.libraryDetails());
+       // library.searchBookInResourcesByObject(book2);
+       // library.searchBookInResourcesByISBN(222);
+
+        //System.out.println(library.searchBookInResourcesByAuthorName("BlueJ"));
 
 
 
